b0e65150116c47ea638b397b1bdd927f12724f79
David Flegal <flegal.david@gmail.com>
2015-11-03 15:00:42 -0800

    first commit

---
+++ jqueer.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+	$('.clothes').draggable();
+});
+
+$(document).ready(function(){
+	$('#back1').click(function(){
+		$('#finn').css('background-image', 'url(img/hell.jpg)');
+	});
+});
+
+$(document).ready(function(){
+	$('#back2').click(function(){
+		$('#finn').css('background-image', 'url(img/oo.png)');
+	});
+});	
+
+$(document).ready(function(){
+	$('#back3').click(function(){
+		$('#finn').css('background-image', 'url(img/beach.jpg)');
+	});
+});	
+
+$(document).ready(function(){
+	$('#back4').click(function(){
+		$('#finn').css('background-image', 'url(img/water.jpg)');
+	});
+});	
+
+$(document).ready(function(){
+	$('#back5').click(function(){
+		$('#finn').css('background-image', 'url(img/mario.png)');
+	});
+});	
